5c28eaf68fe0482414062fba70b605474a9baceb
Shivakumara Bannimatti <shivaslb72@gmail.com>
2020-04-12 15:39:42 +0530

    Updates

---
+++ precache-manifest.d50c3440adc1b014d1caf4c9e7a82749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d42159962f1fe9c241340fefb56108",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "7964e0d1f531e4e1c66a",
+    "url": "/Monster-Rolodex/static/css/main.054579b1.chunk.css"
+  },
+  {
+    "revision": "c07e917bece8e613ec1a",
+    "url": "/Monster-Rolodex/static/js/2.55b8d006.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.55b8d006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7964e0d1f531e4e1c66a",
+    "url": "/Monster-Rolodex/static/js/main.077e0950.chunk.js"
+  },
+  {
+    "revision": "5aae74e9f2efb66f6413",
+    "url": "/Monster-Rolodex/static/js/runtime-main.609a1191.js"
+  }
+]);
